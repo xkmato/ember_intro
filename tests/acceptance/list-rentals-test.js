9f638931753669b2c8e0df26d1d25062b9d40db8
Kenneth <kbonky@gmail.com>
2019-04-15 10:41:52 +0300

    intergration tests

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,35 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import {click, currentURL, visit} from '@ember/test-helpers';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('Should link to information about the company', async function(assert) {
+    await visit('/');
+    await click('.menu-about');
+    assert.equal(currentURL(), '/about', 'should navigate to about')
+  });
+
+  test('Should link to contact information', async function(assert) {
+    await visit('/');
+    await click('.menu-contact');
+    assert.equal(currentURL(), '/contact', 'should navigate to contact')
+  });
+
+  test('Should list available rentals', async function(assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'There are 3 listings')
+  });
+
+  test('Should list available rentals by city', async function(assert) {
+
+  });
+
+  test('Should show details of selected rentals', async function(assert) {});
+
+  test('Should show rentals at the home page', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'Should redirect automatically')
+  });
+});
