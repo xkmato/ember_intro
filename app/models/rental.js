16f3bbf66afdfbefc00692967869143e0c958f76
Kenneth <kbonky@gmail.com>
2019-04-16 08:47:00 +0300

    adding model

---
+++ app/models/rental.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+const { Model } = DS;
+
+export default Model.extend({
+  title: DS.attr(),
+  owner: DS.attr(),
+  city: DS.attr(),
+  category: DS.attr(),
+  image: DS.attr(),
+  bedrooms: DS.attr(),
+  description: DS.attr()
+
+});
