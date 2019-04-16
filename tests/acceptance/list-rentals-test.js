67064535f0aba1b4579b76229cc552ea500f6be0
Kenneth <kbonky@gmail.com>
2019-04-16 09:30:03 +0300

    add filter

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,9 +1,11 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import {click, currentURL, visit} from '@ember/test-helpers';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('Should link to information about the company', async function(assert) {
     await visit('/');
